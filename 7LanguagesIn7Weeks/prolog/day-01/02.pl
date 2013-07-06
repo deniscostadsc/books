25023ee233443b584efb544dc14f0c75add794cd
Denis Costa <deniscostadsc@gmail.com>
2013-07-06 14:26:27 -0300

    One more Prolog problem. I think... 8(

---
+++ 7LanguagesIn7Weeks/prolog/day-01/02.pl
@@ -0,0 +1,15 @@
+musicians_instruments(bb_king, guitar).
+musicians_instruments(john_lee_hooker, guitar).
+musicians_instruments(cliff_burton, bass).
+musicians_instruments(john_bonham, drumms).
+musicians_instruments(john_paul_jones, bass).
+musicians_instruments(robert_plant, vocal).
+
+musicians_style(bb_king, blues).
+musicians_style(john_lee_hooker, blues).
+musicians_style(cliff_burton, rock).
+musicians_style(john_bonham, rock).
+musicians_style(john_paul_jones, rock).
+musicians_style(robert_plant, rock).
+
+%% musicians_instruments(What, guitar).
