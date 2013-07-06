8e2f8baa6cb18f60abab68e97b59b5aadc2b362d
Denis Costa <deniscostadsc@gmail.com>
2013-07-06 14:10:28 -0300

    Adding prolog problems

---
+++ 7LanguagesIn7Weeks/prolog/day-01/01.pl
@@ -0,0 +1,8 @@
+written(douglas_crockford, javascript_the_good_parts).
+written(bruce_tate, seven_language_in_seven_weeks).
+written(alan_moore, v_for_vendetta).
+written(alan_moore, watchmen).
+written(thomas_harris, the_silence_of_the_lambs).
+written(thomas_harris, red_dragon).
+
+%% written(thomas_harris, What).
